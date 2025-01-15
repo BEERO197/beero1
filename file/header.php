c887a40b4671d75c6243829c0daf0d9da5960c34
BEERO197 <mydatab34@gmail.com>
2025-01-15 08:51:22 +0200

    Add files via upload

---
+++ file/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page title</title>
+</head>
+<body>
+    <div class="header">
+        <div class="contianer">
+       <div class="logo">
+      <h1>shoping</h1>   
+      <img src="images/a1.png"></img>  
+     </div>
+       <div class="searsh">
+           <div class="searsh_bar">  
+           <form>
+               <input type="text"  class="searsh_input" name="" placeholder="ادخل كلملة البحث"></input>
+               <button class="button_searsh" name="btn-searsh">بحث</button>
+           </form>
+       </div>
+      </div>  
+    </div> 
+    
+    
+    
+    
+    
+  <div  class="contianer">
+ 
+    <div class="section">
+            
+            <a href="index. html">الرئيسية</a>
+              <a href="index. html">مجوهرات</a>
+               <a href="index. html">الالكترونيات</a>
+                
+              <a href="index. html">ملابس</a>
+              <a href="index. html">عطور</a>
+    </div><!-- نهاية سكشن -->
+   <!--شريط السوشيال ميدا -->  
+    <div class="social">
+        
+            <a href="" target_blank><i class="fa-brands fa-facebook"></i></a>
+            
+             <a href=""><i class="fa-brands fa-twitter"></i></a>
+          
+             
+               <a href="" target_blank><i class="fa-brands fa-instagram"></i></a>
+        
+    </div ><!-- نهاية السوشيال -->  
+    </div><!-- نهاية كونتينر-->
+   
+       <div class="contianer"> 
+     <div class="last-bost"><!--المضاف اخيرا  -->
+      <h4>المضاف اخيرا</h4>      
+       <a href="">
+       <span class="span-img"><img src="images/a4.jpg" width="30"></span> </a>
+      <a href=""> <span class="span-img"><img src="images/a3.jpg" width="30"></span> </a>        
+              
+           <a href="">
+       <span class="span-img"><img src="images/t_shert.jpg" width="30"></span> </a>   
+         <a href="">
+       <span class="span-img"><img src="images/a11.jpg" width="30"></span> </a>
+       
+         <a href="">
+       <span class="span-img"><img src="images/a10.jpg" width="30"></span> </a>
+       
+       
+         
+    </div><!--المضاف اخيرا  نهاية-->
+  
+  
+    <div    class="cart-icon">
+    
+       <i  class="fas fa-shopping-cart" style="font-size:20px" ></i>
+        <span class="cart-count">3</span> <!-- عدد المشتريات -->
+       
+      
+              <a href="sinup.php"><i class="fa-solid fa-user" style="font-size:20px;color:#080808;"></i></a>
+       </div>
+        </div>
+
+</body>
+</html>
